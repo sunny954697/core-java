346e7a272479adebaf81644a11274e17e47d1ff1
sunny954697 <123727511+sunny954697@users.noreply.github.com>
2023-01-27 21:29:45 +0530

    Add files via upload

---
+++ FinalKeyword.java
@@ -0,0 +1,26 @@
+
+class A {
+final int noOfAlphabate=26;
+final int noOfDigit;
+A(){
+noOfDigit=10;
+}
+final void fun()
+{
+System.out.println("Hi,i am function in class A");
+}
+}
+final class B extends A
+{
+// void fun(){
+// System.out.println("Hi,i am function in class A");
+// }
+}
+//class C extends B{
+//
+//}
+public class FinalKeyword{
+    public static void main(String[] args){
+
+    }
+}
