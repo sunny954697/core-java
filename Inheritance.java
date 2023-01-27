871aea3ac6b1827243b121ebe3ff8d634e08ebf5
sunny954697 <123727511+sunny954697@users.noreply.github.com>
2023-01-27 21:36:27 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,36 @@
+//package CoreJava;
+class A{
+int a,b;
+public void show(){
+System.out.println("Hello!");
+}
+}
+//classBextendsA{
+// publicvoidspeak(){
+// System.out.println("Writing!");
+// }
+//}
+interface b{
+public void eat();
+}
+interface D extends b{
+public void sleep();
+}
+class C implements b,D{
+int e,f;
+public void speak(){
+System.out.println("Speaking");
+}
+@Override
+public void eat(){
+}
+@Override
+public void sleep(){
+}
+}
+public class Inheritance{
+public static void main(String[]args){
+// B b=new B();
+// C c=new C();
+}
+}
