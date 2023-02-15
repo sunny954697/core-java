bb52cb0ddeb2a65e494f33575972a47c4ba506f8
sunny954697 <123727511+sunny954697@users.noreply.github.com>
2023-02-15 23:23:31 +0530

    Add files via upload

---
+++ RecursionStudy.java
@@ -0,0 +1,12 @@
+public class RecursionStudy {
+    static void printDescending(int n)
+    {
+    if(n<1) return;
+    System.out.println(n);
+    printDescending(n-1);
+    }
+    public void main(String[] args) {
+    int n = 5;
+    printDescending(n);
+    }
+    }
